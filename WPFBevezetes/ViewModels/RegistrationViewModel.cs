34e79c608f62ec2feee725d98d69dcb1c2d1b022
Egri Patrik <mcklipper@gmail.com>
2021-11-26 19:45:52 +0100

    Adatkötések beállítása.

--- WPFBevezetes/ViewModels/RegistrationViewModel.cs
+++ WPFBevezetes/ViewModels/RegistrationViewModel.cs
@@ -1,10 +1,42 @@
-﻿namespace WPFBevezetes.ViewModels
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace WPFBevezetes.ViewModels
 {
-    public class RegistrationViewModel
+    public class RegistrationViewModel : INotifyPropertyChanged
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+        string _username;
+        public string Username 
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(nameof(Username)); } 
+        }
+
+        string _email;
+        public string Email 
+        {
+            get { return _email;  } 
+            set { _email = value; OnPropertyChanged(nameof(Email)); }
+        }
+
+        SecureString _password;
+        public SecureString Password 
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(nameof(Password)); }
+        }
+
+        SecureString _confirmPassword;
+        public SecureString ConfirmPassword 
+        { 
+            get { return _confirmPassword; }
+            set { _confirmPassword = value; OnPropertyChanged(nameof(ConfirmPassword)); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string callerName = null) =>
+            PropertyChanged?.Invoke(this, null);
     }
 }
--- WPFBevezetes/Views/RegistrationWindow.xaml.cs
+++ WPFBevezetes/Views/RegistrationWindow.xaml.cs
@@ -1,19 +1,57 @@
-﻿using System.Windows;
+﻿using System.Security;
+using System.Windows;
+using System.Windows.Data;
 using WPFBevezetes.ViewModels;
 
 namespace WPFBevezetes.Views
 {
     public partial class RegistrationWindow : Window
     {
+        public DependencyProperty Password =
+            DependencyProperty.RegisterAttached("Password", typeof(SecureString), typeof(RegistrationWindow));
+
+        public DependencyProperty ConfirmPassword =
+            DependencyProperty.RegisterAttached("ConfirmPassword", typeof(SecureString), typeof(RegistrationWindow));
+
+        public RegistrationViewModel Model => 
+            DataContext as RegistrationViewModel;
 
         public RegistrationWindow()
         {
             InitializeComponent();
+
+            pbPassword.SetBinding(Password, new Binding(Password.Name)
+            {
+                Source = Model,
+                Mode = BindingMode.OneWayToSource,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            });
+
+            pbConfirmPassword.SetBinding(ConfirmPassword, new Binding(ConfirmPassword.Name)
+            {
+                Source = Model,
+                Mode = BindingMode.OneWayToSource,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            });
+
         }
 
-        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        private void BtnSignup_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show((DataContext as RegistrationViewModel).Username);
+            MessageBox.Show($"{Model.Username}\n" +
+            $"{Model.Email}\n" +
+            $"{Model.Password.Length}\n" +
+            $"{Model.ConfirmPassword.Length}");
+        }
+
+        private void PbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Model.Password = pbPassword.SecurePassword;
+        }
+
+        private void PbConfirmPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Model.ConfirmPassword = pbConfirmPassword.SecurePassword;
         }
     }
 }
