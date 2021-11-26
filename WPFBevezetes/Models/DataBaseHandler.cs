f41a9e55256b4d69e7ca9c49ae34c31026e26795
Egri Patrik <mcklipper@gmail.com>
2021-11-26 16:47:21 +0100

    Add project files.

---
+++ WPFBevezetes/Models/DataBaseHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WPFBevezetes.Models
+{
+    public static class DataBaseHandler
+    {
+        static SqlConnection connection;
+
+        static DataBaseHandler()
+        {
+            connection = new(@"Data Source=(localdb)\MSSQLLocalDB; " +
+                "Initial Catalog=Wpfbev; Integrated Security=True;");
+        }
+
+        static bool RegistrateUser(object model)
+        {
+            try
+            {
+                connection.Open();
+
+                SqlDataAdapter adapter = new()
+                {
+                    InsertCommand = new($"INSERT INTO users (id, username, email, password) VALUES " +
+                        $"('{ Guid.NewGuid() }', '{ model.ToString() }', '{ model.ToString() }', '{ model.ToString() }')", connection)
+                };
+                adapter.InsertCommand.ExecuteNonQuery();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ WPFBevezetes/Views/RegistrationWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using WPFBevezetes.ViewModels;
+
+namespace WPFBevezetes.Views
+{
+    public partial class RegistrationWindow : Window
+    {
+
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((DataContext as RegistrationViewModel).Username);
+        }
+    }
+}
